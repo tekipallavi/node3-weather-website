6dead13372d8ae5e90c0abd47ecabf6751f8940c
PT-CTS <p@gmail.com>
2024-04-18 19:20:15 +0530

    test

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath));
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Pallavi Teki',
+        name: 'Pallavi Tekku',
         text: 'Use this site to get your weather!!'
     })
 });
