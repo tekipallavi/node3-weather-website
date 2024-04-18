0d812ac7dc7cbd44adf4b6e244b768bdd45202e5
PT <your_email@example.com>
2024-04-18 20:11:45 +0530

    test

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Pallavi Teki'
+        name: 'Pallavi Tekku'
     })
 });
 
